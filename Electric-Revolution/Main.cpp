ed103939aabaf8ecd7dec8957ff710f827c9870f
SKN-JP <sknjpn@gmail.com>
2017-10-17 22:40:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Electric-Revolution/Main.cpp
@@ -0,0 +1,21 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.1.7
+
+void Main()
+{
+	Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+	const Font font(50);
+
+	const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
+
+	while (System::Update())
+	{
+		font(L"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
+
+		font(Cursor::Pos()).draw(20, 400, ColorF(0.6));
+
+		textureCat.resize(80).draw(540, 380);
+
+		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
+	}
+}
